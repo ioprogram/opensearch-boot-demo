b92365ace423d667f1fb34167f82febf55c68833
MG <michele.gallotti88@gmail.com>
2023-02-27 16:08:15 +0100

    Initial commit

---
+++ src/main/java/com/example/opensearchbootdemo/OpensearchBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.opensearchbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensearchBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpensearchBootDemoApplication.class, args);
+	}
+
+}
